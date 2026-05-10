c5bfc8793c831df2ea5870e365e17af3e49ab90b
Bisma Saeed <bismasaeed00@gmail.com>
2026-05-10 16:51:07 +0200

    xcode project created

---
+++ MovieAppMVC/MovieAppMVCApp.swift
@@ -0,0 +1,21 @@
+//
+//  MovieAppMVCApp.swift
+//  MovieAppMVC
+//
+//  Created by Bisma Saeed on 10.05.26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MovieAppMVCApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
